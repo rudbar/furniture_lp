72592059ce86b0cdb73fb638d04e8ac02487cef0
Ian Kachan <kachan.yanf@gmail.com>
2020-05-04 23:47:16 +0500

    Added the partners section with owlcarousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
 
+	$(".carousel-brands").owlCarousel({
+		loop:true,
+		margin: 30,
+		nav: true,
+		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+		responsive:{
+			0:{
+				items:1,
+			},
+			520:{
+				items:1,
+			},
+			560:{
+				items:2,
+			},
+			768:{
+				items:2,
+			},
+			992:{
+				items:3,
+			},
+			1200:{
+				items:4,
+			}
+		}
+	});
+
 	$(".carousel-eq").owlCarousel({
 		loop:true,
 		responsive:{
